f61e174ef54a5654564929701ca0fa651330a19e
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-23 11:02:21 -0500

    search bar

--- client/src/components/BookNavbar.js
+++ client/src/components/BookNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, NavDropdown, Nav } from 'react-bootstrap'
+import {Navbar, NavDropdown, Nav, InputGroup, SplitButton, Dropdown, FormControl} from 'react-bootstrap'
 import './styles/BookNavbar.css'
 
 function BookNavbar () {
@@ -13,6 +13,22 @@ function BookNavbar () {
                     <Nav className='me-auto'>
                         <Nav.Link >Dashboard</Nav.Link>
                         <Nav.Link >Hello</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <InputGroup className='mb-3'>
+                             <FormControl aria-label='some text'/>
+                             <SplitButton
+                             variant='outline-secondary'
+                             title='Search'
+                             id='segmented-button-dropdown-1'
+                             >
+                                 <Dropdown.Item>Book</Dropdown.Item>
+                                 <Dropdown.Item>Author</Dropdown.Item>
+                                 <Dropdown.Item>Genre</Dropdown.Item>
+                                 <Dropdown.Divider />
+                                 <Dropdown.Item>ISBN</Dropdown.Item>
+                             </SplitButton>
+                        </InputGroup>
                     </Nav>
                 </Navbar.Collapse>
                 </div>
