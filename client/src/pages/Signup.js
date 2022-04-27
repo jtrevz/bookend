8e87273f63a0d4b5919ea484ab180c55436474c4
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-27 15:55:01 -0500

    login/signup forms

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import {Row, Col} from 'react-bootstrap'
+import './pages'
 
 function Signup () {
     return (
-        <div> Sign Up</div>
+        <div className='signup'>
+        <Form className='form'>
+        <Row>
+            <Col>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="fName" placeholder="First Name" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                </Form.Group>    
+            </Col>
+            <Col>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="lName" placeholder="Last Name" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+                </Form.Group>
+            </Col>
+        </Row>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
     )
 }
 
