103f352cf6242781ec1f77947f7f5125e7609b8c
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-28 12:32:00 -0500

    log in form done

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -10,28 +10,11 @@ function Login () {
         <Container className='signUpContent'>
         <Row>
             <Col className='col-xs-6 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-6 offset-xl-3'>
-                <h2 className='banner'>Welcome to Bookend!</h2>
+                <h2 className='banner'>Welcome Back!</h2>
                 <Form className='form'>
-                <Row>
-                    <Col>
-                        <Form.Group className="mb-3" >
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="fName" placeholder="First Name" />
-                        </Form.Group>    
-                    </Col>
-                    <Col>
-                        <Form.Group className="mb-3" >
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="lName" placeholder="Last Name" />
-                        </Form.Group>
-                    </Col>
-                </Row>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
