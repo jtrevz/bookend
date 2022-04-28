d05de7365f4f4acec9c75dc06016daf017d94400
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-28 10:53:04 -0500

    signup responsiveness

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -7,43 +7,47 @@ import './../components/styles/Signup.css'
 function Signup () {
     return (
         <div className='signup'>
-        <h1>Sign Up Here!</h1>
         <Container>
-        <Row className="justify-content-md-center">
-        <Form className='form'>
         <Row>
-            <Col>
-                <Form.Group className="mb-3" >
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="fName" placeholder="First Name" />
-                </Form.Group>    
-            </Col>
-            <Col>
-                <Form.Group className="mb-3" >
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lName" placeholder="Last Name" />
+            <Col className='col-xs-6 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-6 offset-xl-3"'>
+            <h2>Welcome to Bookend!</h2>
+            <h5>Create an account or <a href='./login'>log in</a>
+            </h5>
+                <Form className='form'>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" >
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="fName" placeholder="First Name" />
+                        </Form.Group>    
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3" >
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="lName" placeholder="Last Name" />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
                 </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                Submit
+                </Button>
+                </Form>
             </Col>
         </Row>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      </Row>
       </Container>
       </div>
     )
