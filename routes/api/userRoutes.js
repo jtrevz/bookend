7fe2b1e3afb67a916e4d1b497e9f99b0891c6242
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-03 14:39:58 -0500

    sign up user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,15 +1,18 @@
 const router = require("express").Router();
 const userController = require("../../controllers/useController.js");
 
-router.route("/")
-    .get(userController.login)
+router.route("/signup")
     .post(userController.create);
+
+router.route("/login")
+    .get(userController.login)
+
+
 
 router
     .route("/:id")
     .get(userController.findById)
     .put(userController.update)
-    .post(userController.find)
     .delete(tripsController.remove);
 
 module.exports = router;
