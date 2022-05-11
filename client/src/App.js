ec6355fa4231b6d4ba578cafc1e8cc027f506150
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-10 20:44:10 -0500

    dashboard card

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home';
-import Navbar from './components/BookNavbar'
-import Login from './pages/Login/Login'
-import Signup from './pages/Signup/Signup'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/BookNavbar";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
-        <Navbar/>
-        <Router>
-          <Routes>
-          <Route path='/' exact element={<Home />}/>
-          <Route path='/signup' exact element={<Signup/>}/>
-          <Route path='/login' exact element={<Login/>}/>
-          </Routes>
-        </Router>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/home" exact element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
