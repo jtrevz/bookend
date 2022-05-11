30e3904274ec2cd404c1a5f8f0cde912229203d5
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-11 16:13:57 -0500

    dashboard/general setup

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Container, Col, Row, Card } from "react-bootstrap";
 import "./style.css";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <Container className="content">
+        <Row>
+          <Col md={3}>
+            <Card className="userCard">
+              <div className="userImg">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="50%"
+                  height="50%"
+                  fill="currentColor"
+                  class="bi bi-person"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                </svg>
+              </div>
+              <Card.Title>Jenny Trevizo</Card.Title>
+              <Card.Text>friends</Card.Text>
+            </Card>
+          </Col>
+          <Col md={9}>
+            <Card>Dashboard</Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Dashboard;
