bb8f2bfba87c641fb35145f4f382d06e19bed922
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-05-18 12:34:36 -0500

    comment author/timestampt

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -3,7 +3,10 @@ import "./styles/Comment.css";
 function Comment() {
   return (
     <div className="postBack comment">
-      <h5 className="commentAuthor">user123</h5>
+      <div className="authorTimeBox">
+        <h5 className="commentAuthor">jennytrevizo</h5>
+        <h5 className="commentTime"> @ 1:27</h5>
+      </div>
       <p className="commentText">Good observation!</p>
       {/* make comment text to be same font as post text 
       add numbers to like sand comments */}
